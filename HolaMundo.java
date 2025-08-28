8ab42149cad8536296375015e00462f7615dd44c
Rogelio Rojas Santos <rogerojas40@gmail.com>
2025-08-28 15:30:03 -0600

    second commit: now user input its repeated 3 times

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,7 +6,9 @@ public class HolaMundo{
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduce tu nombre: ");
         String nombre = scanner.next();
-        System.out.println("Hola " + nombre);
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Hola " + nombre + "!");
+        }
         System.out.println("¿Cuál es tu edad?");
         int edad = scanner.nextInt();
         System.out.println("Felicidades por esos " + edad + " años");
